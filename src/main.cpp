30499193bb91f3b079afa98be31e1cdb0533dfd3
flo-viseux <frinola.games@gmail.com>
2024-04-19 09:24:35 +0200

    création d'un rectangle

--- src/main.cpp
+++ src/main.cpp
@@ -6,15 +6,20 @@ int main()
     gl::init("TPs de Rendering"); // On crée une fenêtre et on choisit son nom
     gl::maximize_window(); // On peut la maximiser si on veut
 
-    auto const triangle_mesh = gl::Mesh{{
+    auto const rectangle_mesh = gl::Mesh{{
     .vertex_buffers = {{
-        .layout = {gl::VertexAttribute::Position2D{0 /*Index de l'attribut dans le shader, on en reparle juste après*/}},
+        .layout = {gl::VertexAttribute::Position2D{0}},
         .data   = {
-            -1.f, -1.f, // Position2D du 1er sommet
-            +1.f, -1.f, // Position2D du 2ème sommet
-             0.f, +1.f  // Position2D du 3ème sommet
+            -0.5f, -0.5f, // Position2D du 1er sommet
+            +0.5f, -0.5f, // Position2D du 2ème sommet
+            +0.5f, +0.5f, // Position2D du 3ème sommet
+            -0.5f, +0.5f  // Position2D du 4ème sommet
         },
     }},
+    .index_buffer   = {
+        0, 1, 2, // Indices du premier triangle : on utilise le 1er, 2ème et 3ème sommet
+        0, 2, 3  // Indices du deuxième triangle : on utilise le 1er, 3ème et 4ème sommet
+    },
     }};
 
     while (gl::window_is_open())
@@ -24,6 +29,6 @@ int main()
         glClear(GL_COLOR_BUFFER_BIT); // Exécute concrètement l'action d'appliquer sur tout l'écran la couleur choisie au-dessus
 
         gl::bind_default_shader(); // On a besoin qu'un shader soit bind (i.e. "actif") avant de draw(). On en reparle dans la section d'après.
-        triangle_mesh.draw(); // C'est ce qu'on appelle un "draw call" : on envoie l'instruction à la carte graphique de dessiner notre mesh.
+        rectangle_mesh.draw();
     }
 }
