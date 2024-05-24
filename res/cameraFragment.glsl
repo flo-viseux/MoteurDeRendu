d2c61de9892ee55a45e7898a57f397e49c1182d4
Florian Viseux <frinola.games@gmail.com>
2024-05-24 15:28:16 +0200

    shader 3D

--- res/cameraFragment.glsl
+++ res/cameraFragment.glsl
@@ -1,8 +1,10 @@
 #version 410
 
 out vec4 out_color;
+in vec3 vertex_position;
 
 void main()
 {
-    out_color = vec4(0.9, 1.0, 0.0, 1.0);
+    //out_color = vec4(0.9, 1.0, 0.0, 1.0);
+    out_color = vec4(vertex_position, 1.);
 }
--- res/cameraVertex.glsl
+++ res/cameraVertex.glsl
@@ -3,8 +3,10 @@
 layout(location = 0) in vec3 in_position;
 
 uniform mat4 view_projection_matrix;
+out vec3 vertex_position;
 
 void main()
 {
     gl_Position = view_projection_matrix * vec4(in_position, 1.);
+    vertex_position = in_position;
 }
