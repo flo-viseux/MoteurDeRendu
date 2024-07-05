45affa6fc4737c53b20689f916eaee93f0b65a24
Florian Viseux <frinola.games@gmail.com>
2024-07-05 08:29:33 +0200

    post process

--- res/shadowMaskFragment.glsl
+++ res/shadowMaskFragment.glsl
@@ -7,5 +7,12 @@ in vec2 uv;
 void main()
 {
     vec4 texture_color = texture(my_texture, uv);
-    out_color = texture_color;
+
+    //float4 color = tex.Sample(samp, input.Tex);
+    float grey = dot(texture_color.rgb, vec3(.5, .5, .5));
+    vec4 mask = vec4(1, 0, 0, 1);
+
+    vec4 finalColor = texture_color * mask * vec4(vec3(grey), texture_color.a);
+
+    out_color = finalColor;
 }
